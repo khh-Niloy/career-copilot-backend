8ca53549a9a37b1a0a44fff82e041aa28200a5b2
khh-Niloy <niloy.dev.101@gmail.com>
2025-07-06 21:20:13 +0600

    feat: moved PDF-to-text-link extraction to microservice(python)

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
-import multer from "multer";
-// import fs from "fs";
-import pdfParse from "pdf-parse";
+// import multer from "multer";
+// import FormData from "form-data";
+// import axios from "axios";
 
 export const app = express();
 app.use(
@@ -15,27 +15,37 @@ app.use(
 // const upload = multer({ dest: "uploads/" });
 
 // RAM process -> not saving
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+// const storage = multer.memoryStorage();
+// const upload = multer({ storage: storage });
 
-interface MulterRequest extends Request {
-  file: Express.Multer.File;
-}
+// interface MulterRequest extends Request {
+// file: Express.Multer.File;
+// }
 
-app.post("/pdf-to-text", upload.single("pdf"), async (req, res: Response) => {
-  const reqWithFile = req as MulterRequest;
+// app.post("/pdf-to-text", upload.single("pdf"), async (req, res: Response) => {
+// const reqWithFile = req as MulterRequest;
 
-  // ROM process -> saving
-  // const file = reqWithFile.file;
-  // const buffer = fs.readFileSync(file.path);
+// ROM process -> saving
+// const file = reqWithFile.file;
+// const buffer = fs.readFileSync(file.path);
 
-  // RAM process -> not saving
-  const buffer = reqWithFile.file.buffer;
-  const pdfText = await pdfParse(buffer);
+// RAM process -> not saving
+// const pdfBuffer = reqWithFile.file.buffer;
+// const pdfText = await pdfParse(buffer);
 
-  console.log(pdfText.text);
-  res.json({ text: pdfText.text });
-});
+// const formData = new FormData();
+// formData.append("pdf", pdfBuffer, "resume.pdf");
+// const response = await axios.post("http://localhost:9000/extract", formData, {
+//   headers: formData.getHeaders(),
+// });
+
+// const extractedData = response?.data;
+
+// console.log(extractedData);
+
+// console.log(pdfText.text);
+// res.json({ text: extractedData?.text, links: extractedData?.links });
+// });
 
 app.get("/", (req: Request, res: Response) => {
   res.send("welcome to career copilot backend");
