ca096063dd8239b568e91dcb787dbd8148300c95
khh-Niloy <niloy.dev.101@gmail.com>
2025-07-09 00:17:29 +0600

    update: merge fitInterest and WillingnessToLearn field and add words limit

--- src/app.ts
+++ src/app.ts
@@ -39,6 +39,9 @@ app.post(
     const reqWithFile = req as MulterRequest;
     const jobDescription = req.body.jobDescription;
 
+    console.log(jobDescription);
+    console.log(reqWithFile.file);
+
     // //* ROM process -> saving
     // // const file = reqWithFile.file;
     // // const buffer = fs.readFileSync(file.path);
@@ -67,18 +70,28 @@ app.post(
       // console.log(formattedLinks);
 
       const prompt = `Based on my ${resumeContent} and ${formattedLinks} and the uploaded ${jobDescription}, write a short, professional, engaging and tailored email body to apply for the role [ find what job role mentioned in the ${jobDescription}, if multiple mentioned then leave like this (insert your job role) ]. The email should include:
-          1. If the ${jobDescription} specifies how the email subject line should be formatted, follow that exactly. Otherwise, write a clear and professional subject line including my name and the job role I’m applying for.
+          1. If the ${jobDescription} specifies how the email subject line should be formatted, follow that exactly. 
+          Otherwise, write a clear and professional subject line including my name and the job role I’m applying for.
+          
           2. A short, friendly  greeting
-          3. My full name and the specific role I’m applying for [ find what job role mentioned in the ${jobDescription}, if multiple mentioned then leave like this (insert your job role) ]. Keep professional tone.
-          4. My core technical skills relevant to the role and the job, you can find out from my ${resumeContent}
-          5. If the ${jobDescription} doesn’t say to add projects, don’t add them. then dont add but If the ${jobDescription} asks to include any key projects, then find relevant projects from my ${resumeContent} and present them like this:
+
+          3. (NOTE: Please Mention my name part first) Mention My full name earlier and the specific role I’m applying for [ find what job role mentioned in the ${jobDescription}, if multiple mentioned then leave like this (insert your job role) ]. Keep professional tone and Keep it concise max 2 to 2.5 sentences.
+
+          4. My core technical skills relevant to the role and the job, you can find out from my ${resumeContent}. Please dont make long, keep concise and short but not too short!!
+
+          5. -> **NOTE: Be carefull -> If the ${jobDescription} doesn’t say to add projects, don’t add them**. If the ${jobDescription} asks to include any key projects, then find relevant projects from my ${resumeContent} and present them like this:
           [Project Name]: [Live site URL]
-          If the employer requests the code repository or GitHub link, add otherwise dont add:
+          If the employer requests the code repository or GitHub link, add the repo link that you can find from ${formattedLinks} otherwise please dont add:
           Code: [Repository URL]
-          6. A sentence explaining why I’m a strong fit and genuinely interested in the company/role (align this with the ${jobDescription} — this part matters most)
-          7. If the ${jobDescription} includes tools or technologies I haven’t worked with yet (based on my resume), shortly express my genuine interest in learning and working with them. Mention only those are important to write.
-          8. Note that resume is attached and links of my portfolio/GitHub, you can find from here: ${resumeContent} and ${formattedLinks}. resume, github, portfolio
-          9. A polite thank you and closing with my contact info (number and linkedin), you can find from here: ${resumeContent}
+
+          6. A sentence explaining why I’m a strong fit and genuinely interested in the company/role (align this with the ${jobDescription} — this part matters most), Please dont make long, keep short but not too short!!
+
+          If the ${jobDescription} includes tools or technologies I haven’t worked with yet (based on my resume), shortly express my genuine interest in learning and working with them. Mention only those are important to write. Please dont make long, keep short but not too short!!
+
+          You have to merge this two within 35-37 words. And you *have to* focus on the part tools or technologies I haven’t worked with yet (based on my resume).
+
+          7. Note that resume is attached and links of my portfolio/GitHub, you can find from here: ${resumeContent} and ${formattedLinks}. resume, github, portfolio
+          8. A polite thank you and closing with my contact info (number and linkedin), you can find from here: ${resumeContent}
 
           The email should be short, impactful, and respectful of the hiring team’s time or reader’s time — no fluff, no unnecessary lines. Make sure it sounds confident but humble. Avoid generic phrases—make it feel like a real person wrote it, not a template
 
@@ -91,14 +104,13 @@ app.post(
             (dont add if does not say to add projects)projects: [
               {projectName: "", liveSite: "", code: "" }
             ],
-            fitInterest: "",
-            willingnessToLearn: "",
+            fitInterestAndWillingnessToLearn: "",
             attachementsAndLinks: [{
               name: "", links: ""
             }],
             closingAndContact: {
               closing: "",
-              contacts: [{}]
+              contacts: {}
             },
           }
 
@@ -154,8 +166,10 @@ app.post(
             finalText += chunkText;
           }
         }
-
-        res.send(finalText);
+        res.send({
+          aiGeneratedText: JSON.parse(finalText),
+          status: 200,
+        });
       }
 
       geminiAi();
